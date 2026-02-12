63465c801f4f8e4476ca332924cc9aa0a71110b4
KelwinGG <kelwinguilherme77@Gmail.com>
2026-02-11 21:22:42 -0300

    Primeiro código em C# Senai

---
+++ clodoaldo.cs
@@ -0,0 +1,11 @@
+using System; //Biblioteca do C#(system) parao usar comandos de console 
+
+public class Program
+{
+    public static void Main()
+    
+    {
+        Console.WriteLine("Senai");
+    }
+}
+

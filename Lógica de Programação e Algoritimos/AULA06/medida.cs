97ecded7e2dd6dcb84b4f1b9038b6ada5d177e04
KelwinGG <kelwinguilherme77@Gmail.com>
2026-02-23 19:16:58 -0300

    sem try and cat

---
+++ Lógica de Programação e Algoritimos/AULA06/medida.cs
@@ -0,0 +1,20 @@
+ using static System.Console;
+
+double a , b, c , areaQuadrado, areaTriangulo, areaTrapezio;
+ 
+//entrada de dados 
+System.Console.WriteLine("Digite a medida A:");
+a = double.Parse (ReadLine());
+System.Console.WriteLine("Digite a medida B: ");
+b = double.Parse(ReadLine());
+System.Console.WriteLine("Digite a medida C");
+c = double.Parse(ReadLine());
+//Processamento de dados 
+areaQuadrado = a*a;
+areaTriangulo = (a*b) / 2;
+areaTrapezio = ((a + b) * c)/2;
+
+//saida de dados 
+System.Console.WriteLine($"Area do quadrado: {areaQuadrado:F4}");
+System.Console.WriteLine($"Area do Triangulo: {areaTriangulo:F4}");
+System.Console.WriteLine($"Area do Trapezio: {areaTrapezio:F4}");
